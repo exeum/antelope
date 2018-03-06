6384bd7a459842bac767382e882c805109b02772
Christoffer Rehn <crehn@outlook.com>
2018-03-06 15:06:38 +0000

    Fix market price and improve metric structure

--- write.py
+++ write.py
@@ -12,35 +12,20 @@ TIMEOUT = 5
 
 # Gemini, Bitfinex
 def get_prices_type1(obj):
-    return ({order['price'] for order in obj['bids']},
-            {order['price'] for order in obj['asks']})
+    return ({float(order['price']) for order in obj['bids']},
+            {float(order['price']) for order in obj['asks']})
 
 
 # Binance, Okex
 def get_prices_type2(obj):
-    return ({order[0] for order in obj['bids']},
-            {order[0] for order in obj['asks']})
+    return ({float(order[0]) for order in obj['bids']},
+            {float(order[0]) for order in obj['asks']})
 
 
 def write_points(db, points):
     if points:
         print(f'writing {len(points)} points')
         db.write_points(points, time_precision='s')
-
-
-def make_point(exchange, symbol, kind, ts, price):
-    return {
-        'measurement': 'price',
-        'tags': {
-            'exchange': exchange,
-            'symbol': symbol,
-            'kind': kind
-        },
-        'time': int(ts),
-        'fields': {
-            'price': float(price)
-        }
-    }
 
 
 def parse_archive_filename(filename):
@@ -71,9 +56,18 @@ def main():
                 obj = json.loads(line)
                 ts = obj['__timestamp__']
                 bids, asks = get_prices(obj)
-                bid = make_point(exchange, symbol, 'bid', ts, max(bids))
-                ask = make_point(exchange, symbol, 'ask', ts, min(asks))
-                points.extend([bid, ask])
+                points.append({
+                    'measurement': 'price',
+                    'tags': {
+                        'exchange': exchange,
+                        'symbol': symbol
+                    },
+                    'time': int(ts),
+                    'fields': {
+                        'bid': max(bids),
+                        'ask': min(asks)
+                    }
+                })
                 if len(points) >= args.batch_size:
                     write_points(db, points)
                     points = []
