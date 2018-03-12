edc9e20653dbb62880b6f689f82d80be807b34c0
Christoffer Rehn <crehn@outlook.com>
2018-03-12 18:21:48 +0900

    Rename orderbooks volume to data

--- deliverer/deliverer.py
+++ deliverer/deliverer.py
@@ -35,7 +35,7 @@ def main():
     args = parse_args()
     s3 = boto3.client('s3', region_name=args.region)
     while True:
-        paths = [str(path) for path in Path('/orderbooks').glob('orderbook-*[!.gz]')]
+        paths = [str(path) for path in Path('/data').glob('orderbook-*[!.gz]')]
         print(f'currently {len(paths)} order book logs')
         for path in paths:
             if time.strftime('%Y%m%d') not in path:
