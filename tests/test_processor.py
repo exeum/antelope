17e24a9fdf71b9867e29e531bb13e25cc8794b41
Dinh Duong, Mai <dmai@gracenote.com>
2018-03-28 00:35:28 +0900

    + Added unit-tests for process_gdax_book

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -1,5 +1,225 @@
+import json
+import gzip
+from decimal import Decimal
+import time
+from datetime import datetime
+import os
 from processor.processor import normalize
+from processor.processor import read_entries
+from processor.processor import process_gdax_book
 
 
 def test_normalize():
     assert normalize(1.3) == normalize('  0001.3000 ')
+
+
+def e2s(epoch):
+    return datetime.fromtimestamp(epoch).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+
+
+def prepare_sample_gdax_book(fpath, snap_sz, mid_price, limit):
+    assert(snap_sz <= mid_price)
+
+    snapshot = {}
+    base_ts = int(time.time())
+
+    # snapshot
+    snapshot['timestamp'] = float(base_ts) + float(0)
+    snapshot['data'] = {}
+    snapshot['data']['product'] = 'BTC-USD'
+    snapshot['data']['type'] = 'snapshot'
+    bids = []
+    asks = []
+    # snp_entries bids & snap_sz asks in snapshot
+    for i in range(1, snap_sz + 1):
+        bid_price = Decimal(mid_price - i)
+        bid_amount = 1
+        ask_price = Decimal(mid_price + i)
+        ask_amount = 1
+        bids.append([str(bid_price), str(bid_amount)])
+        asks.append([str(ask_price), str(ask_amount)])
+    snapshot['data']['bids'] = bids
+    snapshot['data']['asks'] = asks
+
+    # subscription
+    subscription = {}
+    subscription['timestamp'] = float(base_ts) + float(0)
+    subscription['data'] = {}
+    subscription['data']['type'] = 'subscriptions'
+    channels = []
+    channels.append({'name': 'level2', 'product_ids': ['BTC-USD']})
+    subscription['data']['channels'] = channels
+
+    # update
+    updates = []
+
+    # ts = base_ts + 1 => clear first `limit buys and `limit` sells from mid_price
+    ts = base_ts + 1
+    msec = 1
+    buys_cnt = 0
+    sells_cnt = 0
+    for i in range(0, 2 * limit):
+        update = {}
+        update['timestamp'] = float(ts) + float(msec) / float(1000)
+        update['data'] = {}
+        update['data']['type'] = 'l2update'
+        update['data']['product_id'] = 'BTC-USD'
+        update['data']['time'] = e2s(update['timestamp'])
+        if i % 2 == 0:
+            buys = []
+            buys_cnt += 1
+            buys.append(['buy', str(Decimal(mid_price - buys_cnt)), '0'])
+            update['data']['changes'] = buys
+        else:
+            sells = []
+            sells_cnt += 1
+            sells.append(['sell', str(Decimal(mid_price + sells_cnt)), '0'])
+            update['data']['changes'] = sells
+        msec += 1
+        updates.append(update)
+
+    # ts = base_ts + 2 => take the removed `limit` buys & `limit` sells back
+    ts = base_ts + 2
+    msec = 1
+    buys_cnt = 0
+    sells_cnt = 0
+    for i in range(0, 2 * limit):
+        update = {}
+        update['timestamp'] = float(ts) + float(msec) / float(1000)
+        update['data'] = {}
+        update['data']['type'] = 'l2update'
+        update['data']['product_id'] = 'BTC-USD'
+        update['data']['time'] = e2s(update['timestamp'])
+        if i % 2 == 0:
+            buys = []
+            buys.append(['buy', str(Decimal(mid_price - limit + buys_cnt)), '1'])
+            update['data']['changes'] = buys
+            buys_cnt += 1
+        else:
+            sells = []
+            sells.append(['sell', str(Decimal(mid_price + limit - sells_cnt)), '1'])
+            update['data']['changes'] = sells
+            sells_cnt += 1
+        msec += 1
+        updates.append(update)
+
+    # ts = base_ts + 3 => update amount of
+    # `limit`-highest-price buys & `limit`-lowest-price sells
+    ts = base_ts + 3
+    msec = 1
+    buys_cnt = 0
+    sells_cnt = 0
+    for i in range(0, 2 * limit):
+        update = {}
+        update['timestamp'] = float(ts) + float(msec) / float(1000)
+        update['data'] = {}
+        update['data']['type'] = 'l2update'
+        update['data']['product_id'] = 'BTC-USD'
+        update['data']['time'] = e2s(update['timestamp'])
+        if i % 2 == 0:
+            buys = []
+            buys_cnt += 1
+            buys.append(['buy', str(Decimal(mid_price - buys_cnt)), '2'])
+            update['data']['changes'] = buys
+        else:
+            sells = []
+            sells_cnt += 1
+            sells.append(['sell', str(Decimal(mid_price + sells_cnt)), '2'])
+            update['data']['changes'] = sells
+        msec += 1
+        updates.append(update)
+
+    # ts = base_ts + 4 => update out-of-targeted range
+    ts = base_ts + 4
+    msec = 1
+    buys_cnt = 0
+    sells_cnt = 0
+    for i in range(0, 2 * limit):
+        update = {}
+        update['timestamp'] = float(ts) + float(msec) / float(1000)
+        update['data'] = {}
+        update['data']['type'] = 'l2update'
+        update['data']['product_id'] = 'BTC-USD'
+        update['data']['time'] = e2s(update['timestamp'])
+        if i % 2 == 0:
+            buys = []
+            buys_cnt += 1
+            buys.append(['buy', str(Decimal(mid_price - limit - buys_cnt)), '3'])
+            update['data']['changes'] = buys
+        else:
+            sells = []
+            sells_cnt += 1
+            sells.append(['sell', str(Decimal(mid_price + limit + sells_cnt)), '3'])
+            update['data']['changes'] = sells
+        msec += 1
+        updates.append(update)
+
+    with open(fpath, 'w') as f:
+        f.write(json.dumps(snapshot) + '\n')
+        f.write(json.dumps(subscription) + '\n')
+
+    with open(fpath, 'a') as f:
+        for update in updates:
+            f.write(json.dumps(update) + '\n')
+
+    import shutil
+    with open(fpath, 'rb') as f_in, gzip.open(fpath + '.gz', 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+    return base_ts
+
+
+def test_process_gdax_book():
+    fpath = './gdax-book-btc-usd-20180324-cda678d6c28c454eafbb5dc31c56c354'
+    fpath_gz = fpath + '.gz'
+
+    # remove current files if existed
+    if os.path.exists(fpath):
+        os.remove(fpath)
+    if os.path.exists(fpath_gz):
+        os.remove(fpath_gz)
+
+    # prepare sample data
+    snap_sz = 5000
+    mid_price = 10000
+    limit = 10
+    base_ts = prepare_sample_gdax_book(fpath, snap_sz, mid_price, limit)
+
+    # run unit-tests
+    entries = read_entries(fpath_gz)
+    for (ts, dbids, dasks) in process_gdax_book(entries):
+        sbids = sorted(dbids.items(), key=lambda key_value: key_value[0], reverse=True)
+        sasks = sorted(dasks.items(), key=lambda key_value: key_value[0], reverse=False)
+        bids = sorted(sbids, reverse=True)[:limit]
+        asks = sorted(sasks, reverse=False)[:limit]
+        assert(len(bids) == limit)
+        assert(len(asks) == limit)
+
+        if (ts == base_ts + 1):
+            for i in range(0, limit):
+                assert(bids[i][0] == mid_price - (i + 1))
+                assert(bids[i][1] == '1')
+                assert(asks[i][0] == mid_price + (1 + i))
+                assert(asks[i][1] == '1')
+        elif (ts == base_ts + 2):
+            for i in range(0, limit):
+                assert(bids[i][0] == (mid_price - limit) - (i + 1))
+                assert(bids[i][1] == '1')
+                assert(asks[i][0] == (mid_price + limit) + (i + 1))
+                assert(asks[i][1] == '1')
+        elif (ts == base_ts + 3):
+            for i in range(0, limit):
+                assert(bids[i][0] == mid_price - (i + 1))
+                assert(bids[i][1] == '1')
+                assert(asks[i][0] == mid_price + (1 + i))
+                assert(asks[i][1] == '1')
+        elif (ts == base_ts + 4):
+            for i in range(0, limit):
+                assert(bids[i][0] == mid_price - (i + 1))
+                assert(bids[i][1] == '2')
+                assert(asks[i][0] == mid_price + (1 + i))
+                assert(asks[i][1] == '2')
+
+    # remove created files
+    os.remove(fpath)
+    os.remove(fpath_gz)
