b89b44eca9fabcc5dbd079286d79e48ee1149947
Christoffer Rehn <crehn@outlook.com>
2018-03-28 00:43:25 +0900

    Remove rebuild

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -3,5 +3,6 @@ from processor.processor import normalize
 
 def test_normalize():
     assert normalize(42) == normalize(42.0)
+    assert normalize(1992) == normalize('1992.00000000')
     assert normalize(1.3) == normalize('  0001.3000 ')
     assert normalize(' 012.3400') == normalize('12.34')
